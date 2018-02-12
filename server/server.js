cff7c12e0dbc0d278ffeb08449c97c0791bccb11
Melody <33438150+melodymennen@users.noreply.github.com>
2018-02-12 14:13:38 -0700

    added env example

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
-
+const session = require('express-session');
+ 
 require('dotenv').config();
-
+ 
 const app = express();
-app.use( bodyParser.json() );
-
-
-massive( process.env.CONNECTION_STRING ).then( dbInstance => app.set('db', dbInstance) );
-const port = 3035;
-app.listen( port, () => { console.log(`I'm listening... on Port: ${port}`); } );
+ 
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    saveUninitialized: false, 
+    resave: false
+}));
+ 
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db)
+}).catch(error => {
+    console.log('error', error)
+});
+ 
+const port = process.env.SERVER_PORT
+app.listen(port, () => console.log('listening on port ' + port));
