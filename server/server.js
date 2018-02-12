29acd006103f576afbd377aab6f4d159403ec74c
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-02-12 14:10:25 -0700

    added server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ctr = require('./controllers')
 const massive = require('massive');
 
 require('dotenv').config();
@@ -10,5 +9,5 @@ app.use( bodyParser.json() );
 
 
 massive( process.env.CONNECTION_STRING ).then( dbInstance => app.set('db', dbInstance) );
-const port = 3003;
-app.listen( port, () => { console.log(`I'm listening... on Port: port.`); } );
+const port = 3035;
+app.listen( port, () => { console.log(`I'm listening... on Port: ${port}`); } );
