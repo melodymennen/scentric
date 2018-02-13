28447fce10dc5501b898c84db948c67ea99cfe44
Melody <33438150+melodymennen@users.noreply.github.com>
2018-02-13 12:41:08 -0700

    added links to header, landing page

--- src/components/DisplayProducts.js
+++ src/components/DisplayProducts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
 import axios from 'axios';
 
 class DisplayProducts extends Component {
@@ -28,6 +29,7 @@ class DisplayProducts extends Component {
 
         return (
             <div>
+                <Header />
                 {products}
             </div>
         )
