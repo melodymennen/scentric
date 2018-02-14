c8f102e82c95b270c076fe630121cbfb594910ca
Matt Werdean <mwerdean@yahoo.com>
2018-02-14 10:58:37 -0700

    added nav box shadow and added name slot for auth0

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { login } from '../ducks/reducer'
 import axios from 'axios'
 import Header from './Header'
+import icon from '../styles/images/icons8-customer-26.png'
 
 
 class Login extends Component {
@@ -14,7 +15,17 @@ class Login extends Component {
     }
 
     componentDidMount() {
-        this.lock = new Auth0Lock(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN)
+        var options = {
+            additionalSignUpFields: [{
+              name: "Name",
+              placeholder: "enter your name",
+              // The following properties are optional
+              prefill: "Please Enter Your Name",
+              icon: icon
+            }]
+          }
+
+        this.lock = new Auth0Lock(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN, options)
         this.lock.on('authenticated', authResult => {
             this.lock.getUserInfo(authResult.accessToken, (error, user) => {
                 axios.post('/login', { userId: user.sub }).then(response => {
