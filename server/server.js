ef6609435faae01469b59ff06bbc3d276a95bebd
Matt Werdean <mwerdean@yahoo.com>
2018-02-20 16:46:58 -0700

    cleaned up account

--- server/server.js
+++ server/server.js
@@ -45,6 +45,7 @@ app.get('/api/favorites', controller.getFavorites)
 app.get('/api/ordersall', controller.getAllOrders)
 app.get('/api/allusers', controller.getAllUsers)
 app.delete('/api/favorites/:product_id', controller.removeFavorite)
+app.put('/api/logout' , controller.logout)
 
 app.get('/api/stripeConnect', stripe_ctrl.connect)
 app.post('/api/finalize', stripe_ctrl.finalize)
---
+++ src/components/AccountHistory.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+export default class AccountHistory extends Component {
+    constructor() {
+        super()
+        this.state = {
+            orders: []
+        }
+    }
+    componentDidMount() {
+        axios.get('/api/orders').then((res) => {
+            this.setState({ orders: res.data })
+        })
+    }
+
+
+    render() {
+        const orders = this.state.orders.map(item => {
+            return (
+                <div key={item.id}>
+                    <div>{item.order_subtotal}</div>
+                    <div>{item.order_date}</div>
+                </div>
+            )
+        })
+            return (
+
+                <div className='account_container account_show-favorites'>
+                    <div className="account_history-excerpt">
+                        {orders}
+                    </div>
+                </div>
+
+            );
+        }
+    }
