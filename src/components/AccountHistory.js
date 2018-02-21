b2dff42cd6512b37bb1526f80c82dd6eec33b8fa
Matt Werdean <mwerdean@yahoo.com>
2018-02-20 22:13:34 -0700

    more account cleanliness

--- src/components/AccountHistory.js
+++ src/components/AccountHistory.js
@@ -5,30 +5,32 @@ export default class AccountHistory extends Component {
     constructor() {
         super()
         this.state = {
-            orders: []
+            orders: {}
         }
     }
     componentDidMount() {
         axios.get('/api/orders').then((res) => {
             this.setState({ orders: res.data })
-        })
+            console.log("orders state", this.state.orders)
+            console.log("res.data", res.data)
+        }) 
     }
 
 
     render() {
-        const orders = this.state.orders.map(item => {
-            return (
-                <div key={item.id}>
-                    <div>{item.order_subtotal}</div>
-                    <div>{item.order_date}</div>
-                </div>
-            )
-        })
+        // const orders = this.state.orders.map(item => {
+        //     return (
+        //         <div key={item.id}>
+        //             <div>{item.order_subtotal}</div>
+        //             <div>{item.order_date}</div>
+        //         </div>
+        //     )
+        // })
             return (
 
                 <div className='account_container account_show-favorites'>
                     <div className="account_history-excerpt">
-                        {orders}
+                        {/* { orders } */}
                     </div>
                 </div>
 
