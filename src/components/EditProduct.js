1a2885afb4f2edb05197420be85c80dd96bfa7b1
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-02-20 21:26:11 -0700

    modal

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -7,7 +7,7 @@ class EditProduct extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            product: []
+            product: [],
         }
     }
 
@@ -16,23 +16,39 @@ class EditProduct extends Component {
         this.getProduct(props.id)
     }
 
+    // componentWillMount(props){
+    //     console.log(this.props.id)
+    //     this.getProduct(props.id)
+    // }
+
     getProduct(id){
         axios.get(`/api/admin/${id}`).then(response => {
-            console.log(response.data)
             this.setState({product: response.data[0]})
+            console.log('resosone',response.data)
          })
     }
+
+    
+
     render() {
         const e = this.state.product
+        console.log('df', e)
         return (
-            <div>
-                <div>{e.name}</div>
-                <div>{e.description}</div>
-                <div><img src={e.image_url} alt="product" width="300px"/></div>
-                <div>{e.price}</div>
-                <div>{e.sale ? <div>On Sale</div> : <div>Not on Sale</div>}</div>
-                <div>{e.category}</div>
-                <div>{e.scent_family}</div>
+            <div 
+            // className="edit_products_modal_backdrop"
+            >
+                <div 
+                // className="edit_products_modal"
+                >
+                    <div>{e.name}</div>
+                    <div>{e.description}</div>
+                    <div><img src={e.image_url} alt="product" width="300px"/></div>
+                    <div>{e.price}</div>
+                    <div>{e.sale ? <div>On Sale</div> : <div>Not on Sale</div>}</div>
+                    <div>{e.category}</div>
+                    <div>{e.scent_family}</div>
+                    <button>Close</button>
+                </div>
             </div>
         )
     }
