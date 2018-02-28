69338d548d6a2334ee673f4150bb81a928c4bebd
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-02-28 09:40:51 -0700

    done

--- src/components/dashboardGraphs/Dashboard.js
+++ src/components/dashboardGraphs/Dashboard.js
@@ -8,15 +8,7 @@ import LineSalesByDay from './LineSalesByDay';
 import PurchasedItems from './PurchasedItems'
 
 var today = new Date().toDateString()
-        var curr = new Date()
-        var first = curr.getDate() - curr.getDay()
-        var one = new Date(curr.setDate(first - 1)).toDateString()
-        var two = new Date(curr.setDate(first - 2)).toDateString()
-        var three = new Date(curr.setDate(first - 3)).toDateString()
-        var four = new Date(curr.setDate(first - 4)).toDateString()
-        var five = new Date(curr.setDate(first - 5)).toDateString()
-        var six = new Date(curr.setDate(first - 6)).toDateString()
-        var seven = new Date(curr.setDate(first)).toDateString()
+
 
 class Dashboard extends Component {
     constructor(props) {
@@ -40,9 +32,7 @@ class Dashboard extends Component {
                 orders: response.data
             })
             this.getOrdersToday()
-            this.getOrdersWeek()
             this.getSalesToday()
-            this.getSalesWeek()
         })
     }
 
@@ -59,18 +49,6 @@ class Dashboard extends Component {
         })
     }
 
-    getOrdersWeek(){
-        var countTo = 0
-            this.state.orders.forEach(e => {
-            if(e.order_date === one || two || three || four || five || six || seven){
-            countTo ++
-                }
-            })
-            this.setState({
-                ordersWeek: countTo
-            })
-         }
-
     getSalesToday(){
         var subtotal = 0
             for( let i=0; i< this.state.orders.length; i++){
@@ -86,25 +64,7 @@ class Dashboard extends Component {
                     subtotals: subtotal
                 })
             }
-
-    getSalesWeek(){
-        var weekTotals = 0
-        for( let i=0; i< this.state.orders.length; i++){
-            this.state.orders[i].order_subtotal = +this.state.orders[i].order_subtotal
-        }
-        this.state.orders.forEach(e => {
-            if(e.order_date === one || two || three || four || five || six || seven){
-            weekTotals += e.order_subtotal
-                }
-            })
-            this.setState({
-                weekSubtotals: weekTotals
-            })
-
-
-    }
-    
-    
+ 
 
     render() {
         return (
@@ -121,32 +81,26 @@ class Dashboard extends Component {
                                     <div>Today's Orders</div>
                                     <div>{this.state.todayOrders}</div>
                                 </div>
-                                <div>
-                                    <div>Orders This Week</div>
-                                    <div>{this.state.ordersWeek}</div>
-                                </div>
+
                             </div>
                         </div>
                     </div>
                     <div className="overview_main_body">
                         <div className="dashboard_title">Sales by Day of Week</div>
-                            <div className="order_flex">
+                        <div className="order_flex">
+                            <div>
+                                <LineSalesByDay/>
+                                <BarChartSalesPerWeek/>
+                            </div>
+                            <div>
                                 <div>
-                                    <LineSalesByDay/>
-                                    <BarChartSalesPerWeek/>
+                                    <div>Today's Sales</div>
+                                    <div>$ {this.state.subtotals}</div>
                                 </div>
-                                <div>
-                                    <div>
-                                        <div>Today's Sales</div>
-                                        <div>$ {this.state.subtotals.toFixed(2)}</div>
-                                    </div>
-                                    <div>
-                                        <div>Sales This Week</div>
-                                        <div>$ {this.state.weekSubtotals.toFixed(2)}</div>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
+                    </div>
                     <div className="overview_main_body">
                         <div className="dashboard_title">Top Selling Products</div>
                         <div className="order_flex">
